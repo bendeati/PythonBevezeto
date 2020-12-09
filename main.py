7f4a9dfc7c4968b45f5c6c56065cfa55194e201b
Bende Attila <47759047+bendeati@users.noreply.github.com>
2020-12-09 11:03:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Ez egy komment
+
+print("Helló Világ!")
+print("Ez már az első Python programom!")
+
+# változók létrehozása
+nev = "Bende Attila"
+x = 23
+y = 3.14
+print("A tárolt név: ", nev)
+print("A nev változó típusa: ", type(nev))
+print("A tárolt szám: ", x)
+print("A nev változó típusa: ", type(x))
+print("A tárolt másik szám: ", y)
+print("A nev változó típusa: ", type(y))
+
+szovegHarom = str(3)
+szamHarom = int(3)
+print("A szovegHarom változó típusa: ", type(szovegHarom))
+print("A szamHarom változó típusa: ", type(szamHarom))
+print("2 x 3 = ", szovegHarom * 2)
+print("2 x 3 = ", szamHarom * 2)
+
